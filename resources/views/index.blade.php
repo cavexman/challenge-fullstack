94fdee9e105f554f542466b0bff9039064c61b89
caveman <caveman@mac.com>
2019-12-08 16:57:02 -0800

    fix branding

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,7 +5,7 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <title>Reavel</title>
+  <title>CaveWire</title>
 </head>
 
 <body>
